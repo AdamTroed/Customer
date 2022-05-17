260ae89f44589421f7c0130cb43af7d06088b62d
Adam Gustafsson <adam.troed.carl@hotmail.se>
2022-05-17 19:50:44 +0200

    Add ContactInformation model
    We need to being able to retrieve, create, update and delete contact
    information, hence here we create the model for just that.
    
    The model shall include:
    - Social security number
    - Email address
    - Phone number
    
    Additionally we add an Id to identify each record.
    
    This model will serve as a DTO and a domain model (db) as we do not have
    time to do this properly and separating our endpoint logic and
    the database.

---
+++ Models/ContactInformation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Customer.Models
+{
+    public class ContactInformation
+    {
+        /// <summary>
+        /// Id - unique identifier for each record
+        /// 
+        /// One user can have multiple contact informations
+        /// </summary>
+        public Guid Id { get; set; }
+
+        public int SocialSecurityNumber { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        /// <summary>
+        /// Phone number
+        /// 
+        /// string as it can include country code, e.g. +46
+        /// </summary>
+        public string PhoneNumber { get; set; }
+    }
+}
