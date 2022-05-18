29572cd8e1fa7ac38df8c251dd3fe2cfc42b76f6
Adam Gustafsson <adam.troed.carl@hotmail.se>
2022-05-18 07:53:22 +0200

    Add Swagger and add it to root page
    We want to enable swagger so that we can easily test out and view
    our api, we add it to the root page for simplicity.
    
    While at it, we also change our launch settings to point to
    the root, so swagger is initially called.

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace Customer
 {
@@ -24,6 +25,22 @@ namespace Customer
 
             // use singleton as we want the data to remain the same during the lifetime of the application
             services.AddSingleton<MockContactInformationData>();
+
+            // Register the Swagger Generator service. This service is responsible for genrating Swagger Documents.
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Customer API",
+                    Version = "v1",
+                    Description = "Get customer information",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Adam Gustafsson",
+                        Email = string.Empty,
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +60,19 @@ namespace Customer
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Customer API V1");
+
+                // To serve SwaggerUI at application's root page, set the RoutePrefix property to an empty string.
+                c.RoutePrefix = string.Empty;
             });
         }
     }
